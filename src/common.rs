09405759b109d3a675ec1ddde0497109426c9f2c
yaoyue <yaoyue@fjwr.xyz>
2025-06-11 15:04:22 +0800

    更新api地址

--- src/common.rs
+++ src/common.rs
@@ -996,7 +996,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://rsapi.fjwr.xyz".to_owned()
 }
 
 #[inline]
